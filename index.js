eb711d9f1294c88147feb51b2e5aaa839d9593ac
Waffle <74199677+WaffleChicken86@users.noreply.github.com>
2022-12-12 13:30:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 String.prototype.similarTo=function(text){
-    var spltStr=this.split("")
-    var txtSplt=text.split("")
     var corr=0
     for(let i=0;i<spltStr.length;i++){
-        if(spltStr[i]=txtSplt[i]){
+        if(this.split("")[i]=text.split("")[i]){
             corr+=1
         }
     }
-    if(corr>=Math.round(spltStr.length/2)){
+    if(corr>=this.split("").length/2){
         return true
     }else{
         return false
