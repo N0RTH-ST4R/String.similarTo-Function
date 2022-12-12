5e8fb56107c6fac14f5e9c7257677a52344c4d3a
Waffle <74199677+WaffleChicken86@users.noreply.github.com>
2022-12-12 13:07:19 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+String.prototype.similarTo=function(text){
+    var spltStr=this.split()
+    var txtSplt=text.split()
+    var corr=0
+    for(let i=0;i<spltStr.length;i++){
+        if(spltStr[i]=txtSplt[i]){
+            corr+=1
+        }
+    }
+    if(corr>=Math.round(spltStr.length/2)){
+        return true
+    }else{
+        return false
+    }
+}
